0ec88858f13376a8e4b09ba3c43839e66034bdbc
SwathiKarthick <swathinatarajan@gmail.com>
2022-02-10 18:15:32 +0530

    removed few lines

--- App.js
+++ App.js
@@ -60,7 +60,8 @@ export default class App extends React.Component {
           }}>
           <Text style={styles.buttonText}>GO</Text>
         </TouchableOpacity>
-        <View style= {{flexDirection:"row",alignSelf:"center"}}>
+              
+        <View>
           {this.state.chunks.map((item, index) => {
             return (
               <PhonicSoundButton
@@ -89,7 +90,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
-    borderRadius: 20
+    
   },
   goButton: {
     width: '50%',
